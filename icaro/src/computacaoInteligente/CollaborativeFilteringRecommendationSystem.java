cb636b5fd143b700be7f8b0dd647e71db06d647f
icaro oliveira <icaroliv@hotmail.com>
2023-06-29 19:53:13 -0300

    add folder project

---
+++ icaro/src/computacaoInteligente/CollaborativeFilteringRecommendationSystem.java
@@ -0,0 +1,10 @@
+package computacaoInteligente;
+
+public class CollaborativeFilteringRecommendationSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
